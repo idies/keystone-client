1eb6f5a900d8ee5f9141e779d6d55f92b3091fcb
Laszlo Dobos <dobos@complex.elte.hu>
2014-08-19 19:03:39 -0400

    Added AppSettings to store keystone defaults. Authenticators still take settings from registry!

---
+++ src/Jhu.Graywulf.Keystone/Keystone/AppSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Jhu.Graywulf.Keystone
+{
+    public static class AppSettings
+    {
+        private static string GetValue(string key)
+        {
+            var section = (NameValueCollection)ConfigurationManager.GetSection("Jhu.Graywulf/Keystone");
+            if (section != null)
+            {
+                return (string)section[key];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        public static string Url
+        {
+            get { return GetValue("Url"); }
+        }
+
+        public static string Domain
+        {
+            get { return GetValue("Domain"); }
+        }
+
+        public static string AdminToken
+        {
+            get { return GetValue("AdminToken"); }
+        }
+    }
+}
